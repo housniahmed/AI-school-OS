92af1768dc078b86567d6cf1e5d7d1bfe05f2e10
Ahmed EL HOUSNI <housniforbusiness@gmail.com>
2026-09-11 18:46:52 +0100

    chore(v0.6): remove unused metrics endpoint helper

--- apps/api/src/infra/metrics-endpoint.ts
+++
@@ -1,6 +0,0 @@
-import type { Request, Response } from 'express';
-import { renderMetrics } from './metrics.js';
-
-export function metricsHandler(_req: Request, res: Response) {
-  res.type('text/plain; version=0.0.4; charset=utf-8').send(renderMetrics());
-}
